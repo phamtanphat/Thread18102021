9eb8cbbe3872d262436428baeb68edb35326950a
phamtanphat <phatdroid94@gmail.com>
2022-01-07 18:59:12 +0700

    setup

--- app/src/main/java/com/example/thread18102021/MainActivity.java
+++ app/src/main/java/com/example/thread18102021/MainActivity.java
@@ -8,36 +8,51 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    MyFlag myFlag;
+    int a , b , c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myFlag = new MyFlag(0);
+
+        a = b = c = 0;
 
         Thread threadA = new Thread(new Runnable() {
             @Override
             public void run() {
-                doSthing("A");
+                for (int i = 1; i <= 100; i++) {
+                    a = i;
+                    Log.d("BBB","A : " + a);
+                }
             }
         });
 
         Thread threadB = new Thread(new Runnable() {
             @Override
             public void run() {
-                doSthing("B");
+                for (int i = 1; i <= 100; i++) {
+                    b = i;
+                    Log.d("BBB","B : " + b);
+                }
             }
         });
 
+        Thread threadC = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 100; i++) {
+                    c = a + b;
+                    Log.d("BBB","C : " + c);
+                }
+            }
+        });
+
+        threadC.start();
         threadA.start();
         threadB.start();
 
-    }
-
-    private synchronized void doSthing(String message){
-        for (int i = 0; i < 1000; i++) {
-            Log.d("BBB",message + " : " + i);
-        }
     }
 
 }
---
+++ app/src/main/java/com/example/thread18102021/MyFlag.java
@@ -0,0 +1,9 @@
+package com.example.thread18102021;
+
+public class MyFlag {
+    int index;
+
+    public MyFlag(int index) {
+        this.index = index;
+    }
+}
