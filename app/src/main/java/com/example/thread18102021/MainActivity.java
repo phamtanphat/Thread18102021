c159c925c1258f9795de2088bb02109591d7aff2
phamtanphat <phatdroid94@gmail.com>
2022-01-07 18:41:59 +0700

    synchornized

--- app/src/main/java/com/example/thread18102021/MainActivity.java
+++ app/src/main/java/com/example/thread18102021/MainActivity.java
@@ -8,65 +8,36 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    Object objA , objB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        objA = new Object();
-        objB = new Object();
 
         Thread threadA = new Thread(new Runnable() {
             @Override
             public void run() {
-                synchronized (objA){
-                    Log.d("BBB","Thread A is running");
-                    try {
-                        Thread.sleep(10);
-                        synchronized (objB){
-                            Log.d("BBB","hanlde object B");
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
+                doSthing("A");
             }
         });
 
         Thread threadB = new Thread(new Runnable() {
             @Override
             public void run() {
-                synchronized (objB){
-                    Log.d("BBB","Thread B is running");
-                    try {
-                        Thread.sleep(10);
-                        synchronized (objA){
-                            Log.d("BBB","hanlde object A");
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
+                doSthing("B");
             }
         });
 
         threadA.start();
         threadB.start();
 
-        new CountDownTimer(2000,10){
+    }
 
-            @Override
-            public void onTick(long millisUntilFinished) {
-
-            }
-
-            @Override
-            public void onFinish() {
-                Log.d("BBB","State Thread A " + threadA.getState());
-                Log.d("BBB","State Thread B " + threadB.getState());
-            }
-        }.start();
+    private synchronized void doSthing(String message){
+        for (int i = 0; i < 1000; i++) {
+            Log.d("BBB",message + " : " + i);
+        }
     }
 
 }
